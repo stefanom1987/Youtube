2f3cd1c19bff23f42763dd396a82d2fdf3b6bffa
Stefano <stefano.m1987@outlook.it>
2025-06-17 17:26:50 +0200

    Commit

--- app/components/Videos.tsx
+++ app/components/Videos.tsx
@@ -34,10 +34,7 @@ const Videos = ({
       gap={2}
     >
       {videos.map((item, idx) => (
-        <Box key={idx}>
-          {item.id.videoId && <VideoCard video={item} />}
-          {item.id.channelId && <ChannelCard channelDetail={item} />}
-        </Box>
+        <Box key={idx}>{item.id.videoId && <VideoCard video={item} />}</Box>
       ))}
     </Stack>
   );
